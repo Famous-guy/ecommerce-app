e213204fa7fdb57faaa5230c1c6bbe40f1d03a20
GABRIEL GIDEON BASSEY <140850609+Famous-guy@users.noreply.github.com>
2024-07-30 18:25:47 +0200

    first commit

--- server.js
+++ server.js
@@ -1,4 +1,67 @@
-//initializes
+// const mongoose = require("mongoose");
+// const express = require("express");
+// const cors = require("cors");
+// const path = require("path");
+// const dotenv = require("dotenv");
+// const dotenvExpand = require("dotenv-expand");
+// const myEnv = dotenv.config();
+// dotenvExpand.expand(myEnv);
+
+// //app
+// const app = express();
+
+// //port
+// const port = process.env.PORT || 6400;
+// console.log("Localhost connected on port", port);
+
+// //routes
+// const productRoute = require("./routes/product");
+// const homeRoute = require("./routes/home");
+// const cartRoute = require("./routes/cart");
+// const userRoute = require("./routes/user");
+// const authRoute = require("./routes/auth");
+// const paymentRoute = require("./routes/paymentRoute");
+// const printfulRoute = require("./routes/printfulRoute");
+
+// //middleware
+// app.use(cors({
+//   origin: '*',
+//   credentials: true, // This will not have an effect if you use '*'
+// }));
+// app.use(express.static(path.join(__dirname, "/public")));
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+// //view engine
+// app.set("view engine", "ejs");
+// app.set("views", "views");
+
+// app.disable("view cache");
+
+// app.use("/", homeRoute);
+// app.use("/products", productRoute);
+// app.use("/carts", cartRoute);
+// app.use("/auth", userRoute);
+// app.use("/auth", authRoute);
+// app.use("/payment", paymentRoute);
+// app.use("/printful", printfulRoute);
+
+// //mongoose
+// mongoose.set("useFindAndModify", false);
+// mongoose.set("useUnifiedTopology", true);
+// mongoose
+//   .connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+//   .then(() => {
+//     app.listen(port, () => {
+//       console.log("connect");
+//     });
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+// module.exports = app;
+
 const mongoose = require("mongoose");
 const express = require("express");
 const cors = require("cors");
@@ -25,7 +88,12 @@ const paymentRoute = require("./routes/paymentRoute");
 const printfulRoute = require("./routes/printfulRoute");
 
 //middleware
-app.use(cors({ origin: `https://flowlycart.vercel.app/`, credentials: true }));
+app.use(
+  cors({
+    origin: "*",
+    credentials: true, // This will not have an effect if you use '*'
+  })
+);
 app.use(express.static(path.join(__dirname, "/public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
